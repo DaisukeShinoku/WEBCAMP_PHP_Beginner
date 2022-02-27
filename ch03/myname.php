3d8c4eb777c4c380c1507f770bb5d39036d3079c
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2022-02-27 22:29:36 +0900

    fin_ensyu03

---
+++ ch03/myname.php
@@ -0,0 +1,5 @@
+<?php
+$first_name = "Daisuke";
+$family_name = "Shinoku";
+
+echo "My full name is {$first_name} {$family_name} \n";
---
+++ ch03/tax.php
@@ -0,0 +1,6 @@
+<?php
+$price = 1800;
+$tax_rate = 1.1;
+$price_with_tax = $price * $tax_rate;
+
+echo "{$price}の税込価格は{$price_with_tax} \n";
